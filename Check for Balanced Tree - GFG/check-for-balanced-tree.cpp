d3ff510a98fbac6f4d3c23ff656ef71e2cae3662
rishijain21 <72653234+rishijain21@users.noreply.github.com>
2023-08-21 23:24:32 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -102,49 +102,49 @@ struct Node
  */
 
 class Solution{
-    private:
+    public:
     
-    int height(Node * root){
+    pair<bool,int> isBalancedfast(Node *root){
         
-        //base
-        if(root ==NULL){
-            return 0;
+        
+        if(root==NULL){
+            pair<bool,int> p = make_pair(true,0);
+            return p;
         }
         
-        int left = height(root->left);
-        int right = height(root->right);
         
-        int ans = max(left,right)+1;
+        pair<int,int> left = isBalancedfast(root->left);
+        pair<int,int> right = isBalancedfast(root->right);
+        
+        bool leftans= left.first;
+        bool rightans = right.first;
+        
+        
+        bool diff = abs(left.second - right.second )<= 1;
+        
+        pair<bool,int> ans;
+        
+        
+        ans.second = max(left.second,right.second)+1;
+        
+        
+        if(leftans && rightans && diff){
+            ans.first =true;
+        }
+        else{
+            ans.first = false;
+        }
+        
         return ans;
+        
+        
     }
-    public:
     //Function to check whether a binary tree is balanced or not.
     bool isBalanced(Node *root)
     {
+        
+        return isBalancedfast(root).first;
         //  Your Code here
-        
-        //base case
-        if(root == NULL){
-            return true;
-        }
-        
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        
-        
-        bool diff = abs(height(root->left) - height(root->right) )<=1;
-        
-        if(left && right && diff){
-            return true;
-        }
-        else{
-            return false;
-        }
-
-
-     
-     
-     
     }
 };
 
