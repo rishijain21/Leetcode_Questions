04e7aea446a01e837f766959ff97aac8be6f7028
Rishi Jain <72653234+rishijain21@users.noreply.github.com>
2025-06-09 15:33:16 +0530

    Time: 0 ms (100%), Space: 71.9 MB (17.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         long long sum = 0;
-        int maxi = nums[0];  // Initialize to first element (important!)
-
+        int maxi = nums[0];  
+    
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
 
